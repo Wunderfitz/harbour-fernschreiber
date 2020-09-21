725ba383ca65ae7473f5f16b76c51e2e5e08a636
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-09-21 13:30:49 +0800

    Create harbour-fernschreiber-zh_CN.ts
    Chinese translation finished!

---
+++ translations/harbour-fernschreiber-zh_CN.ts
@@ -0,0 +1,515 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Fernschreiber</source>
+        <translation>关于 Fernschreiber</translation>
+    </message>
+    <message>
+        <source>A Telegram client for Sailfish OS</source>
+        <translation>一个旗鱼系统 Telegram 客户端</translation>
+    </message>
+    <message>
+        <source>By Sebastian J. Wolf</source>
+        <translation>开发者为 Sebastian J. Wolf</translation>
+    </message>
+    <message>
+        <source>Send E-Mail</source>
+        <translation>发送电子邮件</translation>
+    </message>
+    <message>
+        <source>Licensed under GNU GPLv3</source>
+        <translation>采用 GNU GPLv3 许可证</translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation>源代码位于 GitHub</translation>
+    </message>
+    <message>
+        <source>Terms of Service</source>
+        <translation>服务条款</translation>
+    </message>
+    <message>
+        <source>Privacy Policy</source>
+        <translation>隐私政策</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>信誉</translation>
+    </message>
+    <message>
+        <source>This project uses the Telegram Database Library (TDLib). Thanks for making it available under the conditions of the Boost Software License 1.0!</source>
+        <translation>该项目使用 Telegram 数据库（TDLib）。感谢使它在 Boost 软件许可证1.0下可用！</translation>
+    </message>
+    <message>
+        <source>Open Telegram Database Library on GitHub</source>
+        <translation>打开位于 Github 的 Telegram 数据库</translation>
+    </message>
+    <message>
+        <source>About Telegram</source>
+        <translation>关于 Telegram</translation>
+    </message>
+    <message>
+        <source>This product uses the Telegram API but is not endorsed or certified by Telegram.</source>
+        <translation>该项目使用 Telegram API ，但未被 Telegram 批准及背书。</translation>
+    </message>
+    <message>
+        <source>TDLib version %1</source>
+        <translation>TDLib 版本%1</translation>
+    </message>
+    <message>
+        <source>Logged in as %1</source>
+        <translation>登录账号 %1</translation>
+    </message>
+    <message>
+        <source>Phone number: +%1</source>
+        <translation>电话号码：+%1</translation>
+    </message>
+    <message>
+        <source>This project uses twemoji. Copyright 2018 Twitter, Inc. and other contributors. Thanks for making it available under the conditions of the MIT License (coding) and CC-BY 4.0 (graphics)!</source>
+        <translation><该项目使用 twemoji  2018 Twitter, Inc. 版权所有。感谢使它在 MIT 许可证（编码）及 CC-BY 4.0（图像）许可证下可用。</translation>
+    </message>
+    <message>
+        <source>Open twemoji on GitHub</source>
+        <translation>打开位于 Github 的 Twemoji 页面</translation>
+    </message>
+</context>
+<context>
+    <name>BackgroundProgressIndicator</name>
+    <message>
+        <source>%1 %</source>
+        <translation>%1 %</translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+</context>
+<context>
+    <name>ChatPage</name>
+    <message>
+        <source>Unknown</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <source>Your message</source>
+        <translation>你的消息</translation>
+    </message>
+    <message>
+        <source>was never online</source>
+        <translation>完全离线</translation>
+    </message>
+    <message>
+        <source>offline, last online: last month</source>
+        <translation>离线，上次在线时间：上月</translation>
+    </message>
+    <message>
+        <source>offline, last online: last week</source>
+        <translation>离线，上次在线时间：上周</translation>
+    </message>
+    <message>
+        <source>offline, last online: %1</source>
+        <translation>离线，上次在线时间： %1</translation>
+    </message>
+    <message>
+        <source>online</source>
+        <translation>在线</translation>
+    </message>
+    <message>
+        <source>offline, was recently online</source>
+        <translation>离线，最近在线</translation>
+    </message>
+    <message>
+        <source>%1 members, %2 online</source>
+        <translation>%1 位成员, %2 在线</translation>
+    </message>
+    <message>
+        <source>%1 members</source>
+        <translation>%1 位成员</translation>
+    </message>
+    <message>
+        <source>%1M</source>
+        <translation>%1M</translation>
+    </message>
+    <message>
+        <source>%1K</source>
+        <translation>%1T</translation>
+    </message>
+    <message>
+        <source>%1 subscribers</source>
+        <translation>%1 订阅者</translation>
+    </message>
+    <message>
+        <source>Reply to Message</source>
+        <translation>回复该消息</translation>
+    </message>
+    <message>
+        <source>You</source>
+        <translation>你</translation>
+    </message>
+    <message>
+        <source>Loading messages...</source>
+        <translation>加载消息中……</translation>
+    </message>
+    <message>
+        <source>Unmute Chat</source>
+        <translation>取消对话静音</translation>
+    </message>
+    <message>
+        <source>Mute Chat</source>
+        <translation>对话静音</translation>
+    </message>
+    <message>
+        <source>Edit Message</source>
+        <translation>编辑消息</translation>
+    </message>
+    <message>
+        <source>edited</source>
+        <translation>已编辑</translation>
+    </message>
+    <message>
+        <source>Deleting message</source>
+        <translation>正在删除消息</translation>
+    </message>
+    <message>
+        <source>Delete Message</source>
+        <translation>删除消息</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>unread message</source>
+        <translation>未读消息</translation>
+    </message>
+    <message>
+        <source>unread messages</source>
+        <translation>未读消息</translation>
+    </message>
+    <message>
+        <source>in</source>
+        <translation>位于</translation>
+    </message>
+    <message>
+        <source>Waiting for network...</source>
+        <translation>等候网络连接……</translation>
+    </message>
+    <message>
+        <source>Connecting to network...</source>
+        <translation>连接到网络……</translation>
+    </message>
+    <message>
+        <source>Connecting to proxy...</source>
+        <translation>连接到代理……</translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation>已连接</translation>
+    </message>
+    <message>
+        <source>Updating content...</source>
+        <translation>正在更新内容……</translation>
+    </message>
+    <message>
+        <source>chat</source>
+        <translation>对话</translation>
+    </message>
+    <message>
+        <source>chats</source>
+        <translation>对话</translation>
+    </message>
+</context>
+<context>
+    <name>DocumentPreview</name>
+    <message>
+        <source>Download Document</source>
+        <translation>下载文档</translation>
+    </message>
+    <message>
+        <source>Open Document</source>
+        <translation>打开文档</translation>
+    </message>
+</context>
+<context>
+    <name>ImagePage</name>
+    <message>
+        <source>Download Picture</source>
+        <translation>已下载图片</translation>
+    </message>
+    <message>
+        <source>Download of %1 successful.</source>
+        <translation>已成功下载 %1 。</translation>
+    </message>
+    <message>
+        <source>Download failed.</source>
+        <translation>下载失败。</translation>
+    </message>
+</context>
+<context>
+    <name>InReplyToRow</name>
+    <message>
+        <source>You</source>
+        <translation>你的</translation>
+    </message>
+</context>
+<context>
+    <name>InitializationPage</name>
+    <message>
+        <source>OK</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <source>Welcome to Fernschreiber!</source>
+        <translation>欢迎使用 Fernschreiber ！</translation>
+    </message>
+    <message>
+        <source>Please enter your phone number to continue.</source>
+        <translation>请输入你的电话号码以继续。</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>继续</translation>
+    </message>
+    <message>
+        <source>Please enter the code that you received:</source>
+        <translation>请输入你收到的验证码：</translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation>正在加载</translation>
+    </message>
+    <message>
+        <source>Unable to authenticate you with the entered code.</source>
+        <translation>无法使用你输入的验证码进行授权。</translation>
+    </message>
+    <message>
+        <source>Enter code again</source>
+        <translation>再次输入验证码</translation>
+    </message>
+    <message>
+        <source>Restart authentication</source>
+        <translation>重新授权</translation>
+    </message>
+    <message>
+        <source>Please enter your password:</source>
+        <translation>请输入你的密码：</translation>
+    </message>
+</context>
+<context>
+    <name>NotificationManager</name>
+    <message>
+        <source>%1 unread messages</source>
+        <translation>%1 未读消息</translation>
+    </message>
+    <message>
+        <source>sent a picture</source>
+        <translation>发送照片</translation>
+    </message>
+    <message>
+        <source>sent a video</source>
+        <translation>发送视频</translation>
+    </message>
+    <message>
+        <source>sent an animation</source>
+        <translation>发送动画</translation>
+    </message>
+    <message>
+        <source>sent a voice note</source>
+        <translation>发送语音</translation>
+    </message>
+    <message>
+        <source>sent a document</source>
+        <translation>发送文档</translation>
+    </message>
+    <message>
+        <source>sent a location</source>
+        <translation>发送位置</translation>
+    </message>
+    <message>
+        <source>has registered with Telegram</source>
+        <translation>已注册到 Telegram </translation>
+    </message>
+    <message>
+        <source>joined this chat</source>
+        <translation>加入此对话</translation>
+    </message>
+    <message>
+        <source>was added to this chat</source>
+        <translation>已加入此对话</translation>
+    </message>
+    <message>
+        <source>left this chat</source>
+        <translation>离开此对话</translation>
+    </message>
+    <message>
+        <source>Unsupported message: %1</source>
+        <translation>不受支持的消息： %1</translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
+        <source>About Fernschreiber</source>
+        <translation>关于 Fernschreiber</translation>
+    </message>
+    <message>
+        <source>Fernschreiber</source>
+        <translation>Fernschreiber</translation>
+    </message>
+    <message>
+        <source>Waiting for network...</source>
+        <translation>等候网络连接：</translation>
+    </message>
+    <message>
+        <source>Connecting to network...</source>
+        <translation>正在连接到网络……</translation>
+    </message>
+    <message>
+        <source>Connecting to proxy...</source>
+        <translation>正在连接到代理……</translation>
+    </message>
+    <message>
+        <source>Updating content...</source>
+        <translation>正在更新内容……</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <source>You</source>
+        <translation>你</translation>
+    </message>
+    <message>
+        <source>Loading chat list...</source>
+        <translation>正在加载对话列表……</translation>
+    </message>
+    <message>
+        <source>Unmute Chat</source>
+        <translation>取消静音对话</translation>
+    </message>
+    <message>
+        <source>Mute Chat</source>
+        <translation>静音对话</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Behavior</source>
+        <translation>状态</translation>
+    </message>
+    <message>
+        <source>Send message by enter</source>
+        <translation>通过回车键发送消息</translation>
+    </message>
+    <message>
+        <source>Send your message by pressing the enter key</source>
+        <translation>通过按压回车键发送消息</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPage</name>
+    <message>
+        <source>Download Video</source>
+        <translation>下载视频</translation>
+    </message>
+    <message>
+        <source>Download of %1 successful.</source>
+        <translation>已成功下载 %1 。</translation>
+    </message>
+    <message>
+        <source>Download failed.</source>
+        <translation>下载失败</translation>
+    </message>
+</context>
+<context>
+    <name>functions</name>
+    <message>
+        <source>Video: %1</source>
+        <translation>视频 %1</translation>
+    </message>
+    <message>
+        <source>has registered with Telegram</source>
+        <translation>已注册到 Telegram </translation>
+    </message>
+    <message>
+        <source>Picture: %1</source>
+        <translation>照片: %1</translation>
+    </message>
+    <message>
+        <source>Sticker: %1</source>
+        <translation>贴纸: %1</translation>
+    </message>
+    <message>
+        <source>Audio: %1</source>
+        <translation>音频: %1</translation>
+    </message>
+    <message>
+        <source>Voice Note: %1</source>
+        <translation>语音: %1</translation>
+    </message>
+    <message>
+        <source>Animation: %1</source>
+        <translation>动画: %1</translation>
+    </message>
+    <message>
+        <source>Unsupported message: %1</source>
+        <translation>未读消息t: %1</translation>
+    </message>
+    <message>
+        <source>Document: %1</source>
+        <translation>文档: %1</translation>
+    </message>
+    <message>
+        <source>sent a picture</source>
+        <translation>发送照片</translation>
+    </message>
+    <message>
+        <source>sent a video</source>
+        <translation>发送视频</translation>
+    </message>
+    <message>
+        <source>sent an animation</source>
+        <translation>发送动画</translation>
+    </message>
+    <message>
+        <source>sent an audio</source>
+        <translation>发送音频</translation>
+    </message>
+    <message>
+        <source>sent a voice note</source>
+        <translation>发送语言</translation>
+    </message>
+    <message>
+        <source>sent a document</source>
+        <translation>发送文档</translation>
+    </message>
+    <message>
+        <source>sent a location</source>
+        <translation>发送位置</translation>
+    </message>
+    <message>
+        <source>joined this chat</source>
+        <translation>已加入对话</translation>
+    </message>
+    <message>
+        <source>was added to this chat</source>
+        <translation>已加入到此对话</translation>
+    </message>
+    <message>
+        <source>left this chat</source>
+        <translation>离开此对话</translation>
+    </message>
+</context>
+</TS>
