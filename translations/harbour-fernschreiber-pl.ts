54f9c93496b22e246cf20cce41be1383e2628f10
A <hucul@o2.pl>
2020-09-22 11:19:53 +0200

    Create harbour-fernschreiber-pl.ts
    Polish translation

---
+++ translations/harbour-fernschreiber-pl.ts
@@ -0,0 +1,515 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Fernschreiber</source>
+        <translation>O Fernschreiber</translation>
+    </message>
+    <message>
+        <source>A Telegram client for Sailfish OS</source>
+        <translation>Klien Telegram dla Sailfish OS</translation>
+    </message>
+    <message>
+        <source>By Sebastian J. Wolf</source>
+        <translation>Stworzony przez Sebastian J. Wolf</translation>
+    </message>
+    <message>
+        <source>Send E-Mail</source>
+        <translation>Wyślij e-mail</translation>
+    </message>
+    <message>
+        <source>Licensed under GNU GPLv3</source>
+        <translation>Na licencji GNU GPLv3</translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation>Źródła na GitHub</translation>
+    </message>
+    <message>
+        <source>Terms of Service</source>
+        <translation>Warunki usługi</translation>
+    </message>
+    <message>
+        <source>Privacy Policy</source>
+        <translation>Polityka prywatności</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This project uses the Telegram Database Library (TDLib). Thanks for making it available under the conditions of the Boost Software License 1.0!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open Telegram Database Library on GitHub</source>
+        <translation>Otwórz bibliotekę bazy danych Telegram na GitHub</translation>
+    </message>
+    <message>
+        <source>About Telegram</source>
+        <translation>O Telegram</translation>
+    </message>
+    <message>
+        <source>This product uses the Telegram API but is not endorsed or certified by Telegram.</source>
+        <translation>Ten produkt korzysta z Telegram API, ale nie jest zatwierdzony ani certyfikowany przez Telegram.</translation>
+    </message>
+    <message>
+        <source>TDLib version %1</source>
+        <translation>Wersja TDLib %1</translation>
+    </message>
+    <message>
+        <source>Logged in as %1</source>
+        <translation>Zalogowany jako %1</translation>
+    </message>
+    <message>
+        <source>Phone number: +%1</source>
+        <translation>Numer telefonu: +%1</translation>
+    </message>
+    <message>
+        <source>This project uses twemoji. Copyright 2018 Twitter, Inc. and other contributors. Thanks for making it available under the conditions of the MIT License (coding) and CC-BY 4.0 (graphics)!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open twemoji on GitHub</source>
+        <translation>Otwórz twemoji na GitHub</translation>
+    </message>
+</context>
+<context>
+    <name>BackgroundProgressIndicator</name>
+    <message>
+        <source>%1 %</source>
+        <translation>%1 %</translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation>%1</translation>
+    </message>
+</context>
+<context>
+    <name>ChatPage</name>
+    <message>
+        <source>Unknown</source>
+        <translation>Nieznany</translation>
+    </message>
+    <message>
+        <source>Your message</source>
+        <translation>Twoja wiadomość</translation>
+    </message>
+    <message>
+        <source>was never online</source>
+        <translation>nigdy nie był online</translation>
+    </message>
+    <message>
+        <source>offline, last online: last month</source>
+        <translation>offline, ostatnio online: w zeszłym miesiącu</translation>
+    </message>
+    <message>
+        <source>offline, last online: last week</source>
+        <translation>offline, ostatnio online: w zeszłym tygodniu</translation>
+    </message>
+    <message>
+        <source>offline, last online: %1</source>
+        <translation>offline, ostatnio online: %1</translation>
+    </message>
+    <message>
+        <source>online</source>
+        <translation>online</translation>
+    </message>
+    <message>
+        <source>offline, was recently online</source>
+        <translation>offline, był niedawno online</translation>
+    </message>
+    <message>
+        <source>%1 members, %2 online</source>
+        <translation>$1 członków, %2 online </translation>
+    </message>
+    <message>
+        <source>%1 members</source>
+        <translation>%1 czlonków</translation>
+    </message>
+    <message>
+        <source>%1M</source>
+        <translation>%1M</translation>
+    </message>
+    <message>
+        <source>%1K</source>
+        <translation>%1K</translation>
+    </message>
+    <message>
+        <source>%1 subscribers</source>
+        <translation>%1 subskrybentów</translation>
+    </message>
+    <message>
+        <source>Reply to Message</source>
+        <translation>Odpowiedz na wiadomość</translation>
+    </message>
+    <message>
+        <source>You</source>
+        <translation>Ty</translation>
+    </message>
+    <message>
+        <source>Loading messages...</source>
+        <translation>Ładowanie wiadomości...</translation>
+    </message>
+    <message>
+        <source>Unmute Chat</source>
+        <translation>Wyłącz wyciszenie czatu</translation>
+    </message>
+    <message>
+        <source>Mute Chat</source>
+        <translation>Wycisz czat</translation>
+    </message>
+    <message>
+        <source>Edit Message</source>
+        <translation>Edytuj widomość</translation>
+    </message>
+    <message>
+        <source>edited</source>
+        <translation>edytowana</translation>
+    </message>
+    <message>
+        <source>Deleting message</source>
+        <translation>Usuwanie wiadomości</translation>
+    </message>
+    <message>
+        <source>Delete Message</source>
+        <translation>Usuń wiadomość</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>unread message</source>
+        <translation>nieprzeczytana wiadomość</translation>
+    </message>
+    <message>
+        <source>unread messages</source>
+        <translation>nieprzeczytane wiadomości</translation>
+    </message>
+    <message>
+        <source>in</source>
+        <translation>w</translation>
+    </message>
+    <message>
+        <source>Waiting for network...</source>
+        <translation>Oczekiwanie na sieć...</translation>
+    </message>
+    <message>
+        <source>Connecting to network...</source>
+        <translation>Łączenie z siecią...</translation>
+    </message>
+    <message>
+        <source>Connecting to proxy...</source>
+        <translation>Łączenie z proxy</translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation>Połaczony</translation>
+    </message>
+    <message>
+        <source>Updating content...</source>
+        <translation>Aktualizacja treści...</translation>
+    </message>
+    <message>
+        <source>chat</source>
+        <translation>czat</translation>
+    </message>
+    <message>
+        <source>chats</source>
+        <translation>czaty</translation>
+    </message>
+</context>
+<context>
+    <name>DocumentPreview</name>
+    <message>
+        <source>Download Document</source>
+        <translation>Pobierz dokument</translation>
+    </message>
+    <message>
+        <source>Open Document</source>
+        <translation>Otwórz dokument</translation>
+    </message>
+</context>
+<context>
+    <name>ImagePage</name>
+    <message>
+        <source>Download Picture</source>
+        <translation>Pobierz obraz</translation>
+    </message>
+    <message>
+        <source>Download of %1 successful.</source>
+        <translation>Probrano %1 plików</translation>
+    </message>
+    <message>
+        <source>Download failed.</source>
+        <translation>Nieudane pobranie</translation>
+    </message>
+</context>
+<context>
+    <name>InReplyToRow</name>
+    <message>
+        <source>You</source>
+        <translation>Ty</translation>
+    </message>
+</context>
+<context>
+    <name>InitializationPage</name>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>Welcome to Fernschreiber!</source>
+        <translation>Witamy w Fernschreiber!</translation>
+    </message>
+    <message>
+        <source>Please enter your phone number to continue.</source>
+        <translation>Podaj swój numer telefonu, aby kontynuować.</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Kontynuuj</translation>
+    </message>
+    <message>
+        <source>Please enter the code that you received:</source>
+        <translation>Wpisz otrzymany kod:</translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation>Ładowanie...</translation>
+    </message>
+    <message>
+        <source>Unable to authenticate you with the entered code.</source>
+        <translation>Nie można uwierzytelnić Ciebie wprowadzonym kodem.</translation>
+    </message>
+    <message>
+        <source>Enter code again</source>
+        <translation>Wprowadź ponownie kod</translation>
+    </message>
+    <message>
+        <source>Restart authentication</source>
+        <translation>Uruchom ponownie uwierzytelnianie</translation>
+    </message>
+    <message>
+        <source>Please enter your password:</source>
+        <translation>Wprowadź hasło:</translation>
+    </message>
+</context>
+<context>
+    <name>NotificationManager</name>
+    <message>
+        <source>%1 unread messages</source>
+        <translation>%1 nieprzeczytanych wiadomości</translation>
+    </message>
+    <message>
+        <source>sent a picture</source>
+        <translation>wyślij obraz</translation>
+    </message>
+    <message>
+        <source>sent a video</source>
+        <translation>wyślij film</translation>
+    </message>
+    <message>
+        <source>sent an animation</source>
+        <translation>wyślij animację</translation>
+    </message>
+    <message>
+        <source>sent a voice note</source>
+        <translation>wyślij notatke głosową</translation>
+    </message>
+    <message>
+        <source>sent a document</source>
+        <translation>wyślij dokument</translation>
+    </message>
+    <message>
+        <source>sent a location</source>
+        <translation>wyślij lokalizację</translation>
+    </message>
+    <message>
+        <source>has registered with Telegram</source>
+        <translation>zarejestrował się w Telegramie</translation>
+    </message>
+    <message>
+        <source>joined this chat</source>
+        <translation>dołączył do tego czatu</translation>
+    </message>
+    <message>
+        <source>was added to this chat</source>
+        <translation>został dodany do tego czatu</translation>
+    </message>
+    <message>
+        <source>left this chat</source>
+        <translation>opuścil ten czat</translation>
+    </message>
+    <message>
+        <source>Unsupported message: %1</source>
+        <translation>Nieobsługiwana wiadomość: %1</translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
+        <source>About Fernschreiber</source>
+        <translation>O Fernschreiber</translation>
+    </message>
+    <message>
+        <source>Fernschreiber</source>
+        <translation>Fernschreiber</translation>
+    </message>
+    <message>
+        <source>Waiting for network...</source>
+        <translation>Czekanie na sieć</translation>
+    </message>
+    <message>
+        <source>Connecting to network...</source>
+        <translation>Łączenie z siecią...</translation>
+    </message>
+    <message>
+        <source>Connecting to proxy...</source>
+        <translation>Łączenie z proxy</translation>
+    </message>
+    <message>
+        <source>Updating content...</source>
+        <translation>Aktualizacja treści...</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Nieznany</translation>
+    </message>
+    <message>
+        <source>You</source>
+        <translation>Ty</translation>
+    </message>
+    <message>
+        <source>Loading chat list...</source>
+        <translation>Ładowanie listy czatu...</translation>
+    </message>
+    <message>
+        <source>Unmute Chat</source>
+        <translation>Wyłącz wyciszenie czatu</translation>
+    </message>
+    <message>
+        <source>Mute Chat</source>
+        <translation>Wycisz czat</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+    <message>
+        <source>Behavior</source>
+        <translation>Zachowanie</translation>
+    </message>
+    <message>
+        <source>Send message by enter</source>
+        <translation>Wyślij wiadomość przez naciśniecie enter</translation>
+    </message>
+    <message>
+        <source>Send your message by pressing the enter key</source>
+        <translation>Wyślij wiadomość przez naciśniecie przycisku enter</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPage</name>
+    <message>
+        <source>Download Video</source>
+        <translation>Pobierz film</translation>
+    </message>
+    <message>
+        <source>Download of %1 successful.</source>
+        <translation>Pobieranie %1 zakończone sukcesem</translation>
+    </message>
+    <message>
+        <source>Download failed.</source>
+        <translation>Nieudane pobieranie</translation>
+    </message>
+</context>
+<context>
+    <name>functions</name>
+    <message>
+        <source>Video: %1</source>
+        <translation>Film: %1</translation>
+    </message>
+    <message>
+        <source>has registered with Telegram</source>
+        <translation>zarejestrował się w Telegramie</translation>
+    </message>
+    <message>
+        <source>Picture: %1</source>
+        <translation>Obraz: %1</translation>
+    </message>
+    <message>
+        <source>Sticker: %1</source>
+        <translation>Naklejka: %1</translation>
+    </message>
+    <message>
+        <source>Audio: %1</source>
+        <translation>Dźwięk: %1</translation>
+    </message>
+    <message>
+        <source>Voice Note: %1</source>
+        <translation>Notatka głosowa: %1</translation>
+    </message>
+    <message>
+        <source>Animation: %1</source>
+        <translation>Animacja: %1</translation>
+    </message>
+    <message>
+        <source>Unsupported message: %1</source>
+        <translation>Nieobsługiwana wiadomość: %1</translation>
+    </message>
+    <message>
+        <source>Document: %1</source>
+        <translation>Dokument: %1</translation>
+    </message>
+    <message>
+        <source>sent a picture</source>
+        <translation>wyślij obraz</translation>
+    </message>
+    <message>
+        <source>sent a video</source>
+        <translation>wyślij film</translation>
+    </message>
+    <message>
+        <source>sent an animation</source>
+        <translation>wyślij animację</translation>
+    </message>
+    <message>
+        <source>sent an audio</source>
+        <translation>wyślij dźwięk</translation>
+    </message>
+    <message>
+        <source>sent a voice note</source>
+        <translation>wyślij notatke głosową</translation>
+    </message>
+    <message>
+        <source>sent a document</source>
+        <translation>wyślij dokument</translation>
+    </message>
+    <message>
+        <source>sent a location</source>
+        <translation>wyślij lokalizację</translation>
+    </message>
+    <message>
+        <source>joined this chat</source>
+        <translation>dołaczył do tego czatu</translation>
+    </message>
+    <message>
+        <source>was added to this chat</source>
+        <translation>został dodany do tego czatu</translation>
+    </message>
+    <message>
+        <source>left this chat</source>
+        <translation>opuścił ten czat</translation>
+    </message>
+</context>
+</TS>
