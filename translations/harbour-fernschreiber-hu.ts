27691ea70c3de03f8f4ca2466bc150f7e643ef91
edp17 <edp17@pm.me>
2020-09-23 02:20:55 +0100

    hungarian translation

---
+++ translations/harbour-fernschreiber-hu.ts
@@ -0,0 +1,515 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Fernschreiber</source>
+        <translation type="A Fernschreiber névjegye"></translation>
+    </message>
+    <message>
+        <source>A Telegram client for Sailfish OS</source>
+        <translation type="Sailfish OS Telegram kliens"></translation>
+    </message>
+    <message>
+        <source>By Sebastian J. Wolf</source>
+        <translation type="Készítette Sebastian J. Wolf"></translation>
+    </message>
+    <message>
+        <source>Send E-Mail</source>
+        <translation type="E-mail küldés"></translation>
+    </message>
+    <message>
+        <source>Licensed under GNU GPLv3</source>
+        <translation type="Licensz: GNU GPLv3"></translation>
+    </message>
+    <message>
+        <source>Sources on GitHub</source>
+        <translation type="Forráskód elérhető a GitHub-on"></translation>
+    </message>
+    <message>
+        <source>Terms of Service</source>
+        <translation type="A szolgáltatás feltételei"></translation>
+    </message>
+    <message>
+        <source>Privacy Policy</source>
+        <translation type="Adatvédelmi irányelvek"></translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation type="Készítők"></translation>
+    </message>
+    <message>
+        <source>This project uses the Telegram Database Library (TDLib). Thanks for making it available under the conditions of the Boost Software License 1.0!</source>
+        <translation type="Ez a projekt a Telegram Adatkönyvtárat (TDLib) használja. Köszönet amiért elérhetővé tették a Boost Software License 1.0 licensz feltételei alatt!"></translation>
+    </message>
+    <message>
+        <source>Open Telegram Database Library on GitHub</source>
+        <translation type="Nyitott Telegram Adatkönyvtár a GitHub-on"></translation>
+    </message>
+    <message>
+        <source>About Telegram</source>
+        <translation type="A Telegram névjegye"></translation>
+    </message>
+    <message>
+        <source>This product uses the Telegram API but is not endorsed or certified by Telegram.</source>
+        <translation type="Ez a projekt, bár a Telegram API-kat használja, de sem jóváhagyva sem tanúsítva nincs a Telegram által."></translation>
+    </message>
+    <message>
+        <source>TDLib version %1</source>
+        <translation type="TDLib verzió %1"></translation>
+    </message>
+    <message>
+        <source>Logged in as %1</source>
+        <translation type="Bejelentkezve mint %1"></translation>
+    </message>
+    <message>
+        <source>Phone number: +%1</source>
+        <translation type="Telefonszám: +%1"></translation>
+    </message>
+    <message>
+        <source>This project uses twemoji. Copyright 2018 Twitter, Inc. and other contributors. Thanks for making it available under the conditions of the MIT License (coding) and CC-BY 4.0 (graphics)!</source>
+        <translation type="Ez a projekt a twemoji-kat használja. Szerzői jog 2018 Twitter, Inc. és egyéb együttműködők. Köszönet amiért elérhetővé tették az MIT Licensz (programozás) és a CC-BY 4.0 (grafika) feltételei alatt!"></translation>
+    </message>
+    <message>
+        <source>Open twemoji on GitHub</source>
+        <translation type="Nyitott twemoji a GitHub-on"></translation>
+    </message>
+</context>
+<context>
+    <name>BackgroundProgressIndicator</name>
+    <message>
+        <source>%1 %</source>
+        <translation type="%1 %"></translation>
+    </message>
+    <message>
+        <source>%1</source>
+        <translation type="%1"></translation>
+    </message>
+</context>
+<context>
+    <name>ChatPage</name>
+    <message>
+        <source>Unknown</source>
+        <translation type="Ismeretlen"></translation>
+    </message>
+    <message>
+        <source>Your message</source>
+        <translation type="A te üzeneted"></translation>
+    </message>
+    <message>
+        <source>was never online</source>
+        <translation type="soha nem volt online"></translation>
+    </message>
+    <message>
+        <source>offline, last online: last month</source>
+        <translation type="offline, utoljára online: múlt hónapban"></translation>
+    </message>
+    <message>
+        <source>offline, last online: last week</source>
+        <translation type="offline, utoljára online: múlt héten"></translation>
+    </message>
+    <message>
+        <source>offline, last online: %1</source>
+        <translation type="offline, utoljára online: %1"></translation>
+    </message>
+    <message>
+        <source>online</source>
+        <translation type="online"></translation>
+    </message>
+    <message>
+        <source>offline, was recently online</source>
+        <translation type="offline, nemrégen volt online"></translation>
+    </message>
+    <message>
+        <source>%1 members, %2 online</source>
+        <translation type="%1 tag(ok), %2 online"></translation>
+    </message>
+    <message>
+        <source>%1 members</source>
+        <translation type="%1 tag(ok)"></translation>
+    </message>
+    <message>
+        <source>%1M</source>
+        <translation type="%1M"></translation>
+    </message>
+    <message>
+        <source>%1K</source>
+        <translation type="%1K"></translation>
+    </message>
+    <message>
+        <source>%1 subscribers</source>
+        <translation type="%1 előfizető"></translation>
+    </message>
+    <message>
+        <source>Reply to Message</source>
+        <translation type="Válasz az üzenetre"></translation>
+    </message>
+    <message>
+        <source>You</source>
+        <translation type="Te"></translation>
+    </message>
+    <message>
+        <source>Loading messages...</source>
+        <translation type="Üzenetek betöltése..."></translation>
+    </message>
+    <message>
+        <source>Unmute Chat</source>
+        <translation type="Csevegés némítás feloldása"></translation>
+    </message>
+    <message>
+        <source>Mute Chat</source>
+        <translation type="Csevegés némítása"></translation>
+    </message>
+    <message>
+        <source>Edit Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>edited</source>
+        <translation type="Üzenet szerkesztése"></translation>
+    </message>
+    <message>
+        <source>Deleting message</source>
+        <translation type="Üzenet törlése"></translation>
+    </message>
+    <message>
+        <source>Delete Message</source>
+        <translation type="Üzenet törlése"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>unread message</source>
+        <translation type="olvasatlan üzenet"></translation>
+    </message>
+    <message>
+        <source>unread messages</source>
+        <translation type="olvasatlan üzenetek"></translation>
+    </message>
+    <message>
+        <source>in</source>
+        <translation type="ban"></translation>
+    </message>
+    <message>
+        <source>Waiting for network...</source>
+        <translation type="Hálózatra várakozás..."></translation>
+    </message>
+    <message>
+        <source>Connecting to network...</source>
+        <translation type="Csatlakozás a hálózathoz..."></translation>
+    </message>
+    <message>
+        <source>Connecting to proxy...</source>
+        <translation type="Csatlakozás a proxy-hoz..."></translation>
+    </message>
+    <message>
+        <source>Connected</source>
+        <translation type="Csatlakozva"></translation>
+    </message>
+    <message>
+        <source>Updating content...</source>
+        <translation type="Tartalom frissítése..."></translation>
+    </message>
+    <message>
+        <source>chat</source>
+        <translation type="csevegés"></translation>
+    </message>
+    <message>
+        <source>chats</source>
+        <translation type="csevegések"></translation>
+    </message>
+</context>
+<context>
+    <name>DocumentPreview</name>
+    <message>
+        <source>Download Document</source>
+        <translation type="Dokumentum letöltése"></translation>
+    </message>
+    <message>
+        <source>Open Document</source>
+        <translation type="Dokumentum megyitása"></translation>
+    </message>
+</context>
+<context>
+    <name>ImagePage</name>
+    <message>
+        <source>Download Picture</source>
+        <translation type="Kép letöltése"></translation>
+    </message>
+    <message>
+        <source>Download of %1 successful.</source>
+        <translation type="A %1 letöltése sikerült."></translation>
+    </message>
+    <message>
+        <source>Download failed.</source>
+        <translation type="A letöltés nem sikerült."></translation>
+    </message>
+</context>
+<context>
+    <name>InReplyToRow</name>
+    <message>
+        <source>You</source>
+        <translation type="Te"></translation>
+    </message>
+</context>
+<context>
+    <name>InitializationPage</name>
+    <message>
+        <source>OK</source>
+        <translation type="Rendben"></translation>
+    </message>
+    <message>
+        <source>Welcome to Fernschreiber!</source>
+        <translation type="Üdvözöl a Fernschreiber!"></translation>
+    </message>
+    <message>
+        <source>Please enter your phone number to continue.</source>
+        <translation type="Kérlek add meg a telefonszámodat a folytatáshoz."></translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation type="Folytatás"></translation>
+    </message>
+    <message>
+        <source>Please enter the code that you received:</source>
+        <translation type="Kérlek add meg a kapott kódot:"></translation>
+    </message>
+    <message>
+        <source>Loading...</source>
+        <translation type="Betöltés..."></translation>
+    </message>
+    <message>
+        <source>Unable to authenticate you with the entered code.</source>
+        <translation type="A megadott kód azonosítása sikertelen."></translation>
+    </message>
+    <message>
+        <source>Enter code again</source>
+        <translation type="Kód ismételt megadása"></translation>
+    </message>
+    <message>
+        <source>Restart authentication</source>
+        <translation type="Azonosítás újrakezédse"></translation>
+    </message>
+    <message>
+        <source>Please enter your password:</source>
+        <translation type="Kérlek add meg a jelszavad:"></translation>
+    </message>
+</context>
+<context>
+    <name>NotificationManager</name>
+    <message>
+        <source>%1 unread messages</source>
+        <translation type="%1 olvasatlan üzenet"></translation>
+    </message>
+    <message>
+        <source>sent a picture</source>
+        <translation type="képet küldött"></translation>
+    </message>
+    <message>
+        <source>sent a video</source>
+        <translation type="videót küldött"></translation>
+    </message>
+    <message>
+        <source>sent an animation</source>
+        <translation type="animációt küldött"></translation>
+    </message>
+    <message>
+        <source>sent a voice note</source>
+        <translation type="hangfelvételt küldött"></translation>
+    </message>
+    <message>
+        <source>sent a document</source>
+        <translation type="dokumentumok küldött"></translation>
+    </message>
+    <message>
+        <source>sent a location</source>
+        <translation type="helyzetjelentést küldött"></translation>
+    </message>
+    <message>
+        <source>has registered with Telegram</source>
+        <translation type="regisztrált a Telegram-al"></translation>
+    </message>
+    <message>
+        <source>joined this chat</source>
+        <translation type="csatlakozott a csevegéshez"></translation>
+    </message>
+    <message>
+        <source>was added to this chat</source>
+        <translation type="hozzáadva a csevegéshez"></translation>
+    </message>
+    <message>
+        <source>left this chat</source>
+        <translation type="kilépett a csevegésből"></translation>
+    </message>
+    <message>
+        <source>Unsupported message: %1</source>
+        <translation type="Nem támogatott üzenet: %1"></translation>
+    </message>
+</context>
+<context>
+    <name>OverviewPage</name>
+    <message>
+        <source>About Fernschreiber</source>
+        <translation type="A Fernschreiber névjegye"></translation>
+    </message>
+    <message>
+        <source>Fernschreiber</source>
+        <translation type="Fernschreiber"></translation>
+    </message>
+    <message>
+        <source>Waiting for network...</source>
+        <translation type="Hálózatra várakozás..."></translation>
+    </message>
+    <message>
+        <source>Connecting to network...</source>
+        <translation type="Csatlakozás a hálózathoz..."></translation>
+    </message>
+    <message>
+        <source>Connecting to proxy...</source>
+        <translation type="Csatlakozás a proxy-hoz..."></translation>
+    </message>
+    <message>
+        <source>Updating content...</source>
+        <translation type="Tartalom frissítése..."></translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation type="Ismeretlen"></translation>
+    </message>
+    <message>
+        <source>You</source>
+        <translation type="Te"></translation>
+    </message>
+    <message>
+        <source>Loading chat list...</source>
+        <translation type="Csevegés lista betöltése..."></translation>
+    </message>
+    <message>
+        <source>Unmute Chat</source>
+        <translation type="Csevegés némítás feloldása"></translation>
+    </message>
+    <message>
+        <source>Mute Chat</source>
+        <translation type="Csevegés némítása"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="Beállítások"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation type="Beállítások"></translation>
+    </message>
+    <message>
+        <source>Behavior</source>
+        <translation type="Működés"></translation>
+    </message>
+    <message>
+        <source>Send message by enter</source>
+        <translation type="Üzenet küldése Enter-rel"></translation>
+    </message>
+    <message>
+        <source>Send your message by pressing the enter key</source>
+        <translation type="Üzenet küldése az enter gomb lenyomásával"></translation>
+    </message>
+</context>
+<context>
+    <name>VideoPage</name>
+    <message>
+        <source>Download Video</source>
+        <translation type="Videó letöltése"></translation>
+    </message>
+    <message>
+        <source>Download of %1 successful.</source>
+        <translation type="A %1 letöltése sikerült."></translation>
+    </message>
+    <message>
+        <source>Download failed.</source>
+        <translation type="A letöltés nem sikerült."></translation>
+    </message>
+</context>
+<context>
+    <name>functions</name>
+    <message>
+        <source>Video: %1</source>
+        <translation type="Videó %1"></translation>
+    </message>
+    <message>
+        <source>has registered with Telegram</source>
+        <translation type="regisztrált a Telegram-al"></translation>
+    </message>
+    <message>
+        <source>Picture: %1</source>
+        <translation type="Kép: %1"></translation>
+    </message>
+    <message>
+        <source>Sticker: %1</source>
+        <translation type="Matrica: %1"></translation>
+    </message>
+    <message>
+        <source>Audio: %1</source>
+        <translation type="Audió: %1"></translation>
+    </message>
+    <message>
+        <source>Voice Note: %1</source>
+        <translation type="Hang üzenet: %1"></translation>
+    </message>
+    <message>
+        <source>Animation: %1</source>
+        <translation type="Animáció: %1"></translation>
+    </message>
+    <message>
+        <source>Unsupported message: %1</source>
+        <translation type="Nem támogatott üzenet: %1"></translation>
+    </message>
+    <message>
+        <source>Document: %1</source>
+        <translation type="Dokument: %1"></translation>
+    </message>
+    <message>
+        <source>sent a picture</source>
+        <translation type="képet küldött"></translation>
+    </message>
+    <message>
+        <source>sent a video</source>
+        <translation type="videót küldött"></translation>
+    </message>
+    <message>
+        <source>sent an animation</source>
+        <translation type="animációt küldött"></translation>
+    </message>
+    <message>
+        <source>sent an audio</source>
+        <translation type="audiót küldött"></translation>
+    </message>
+    <message>
+        <source>sent a voice note</source>
+        <translation type="hangfelvételt küldött"></translation>
+    </message>
+    <message>
+        <source>sent a document</source>
+        <translation type="dokumentumot küldött"></translation>
+    </message>
+    <message>
+        <source>sent a location</source>
+        <translation type="helyzetjelentést küldött"></translation>
+    </message>
+    <message>
+        <source>joined this chat</source>
+        <translation type="csatlakozott a csevegéshez"></translation>
+    </message>
+    <message>
+        <source>was added to this chat</source>
+        <translation type="hozzáadva a csevegéshez"></translation>
+    </message>
+    <message>
+        <source>left this chat</source>
+        <translation type="kilépett a csevegésből"></translation>
+    </message>
+</context>
+</TS>
