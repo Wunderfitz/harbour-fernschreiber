f2ccb86a53cfa6dd5e24fafd70609ea20255e1c3
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-11-23 17:29:10 +0800

    Update harbour-fernschreiber-zh_CN.ts

--- translations/harbour-fernschreiber-zh_CN.ts
+++ translations/harbour-fernschreiber-zh_CN.ts
@@ -1181,7 +1181,7 @@
     </message>
     <message>
         <source>Notification turns on the display</source>
-        <translation type="unfinished"></translation>
+        <translation>收到通知时点亮屏幕</translation>
     </message>
 </context>
 <context>
